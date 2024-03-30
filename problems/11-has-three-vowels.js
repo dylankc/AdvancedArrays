a437856b7f8b218c0e295c1afdabca9f31c7a1d9
Cannicakee <stanfill11@gmail.com>
2024-03-30 16:26:38 -0600

    Completed Problem 11

--- problems/11-has-three-vowels.js
+++ problems/11-has-three-vowels.js
@@ -16,12 +16,44 @@ console.log(hasThreeVowels('go home'));         //  false
 
 */
 
-let hasThreeVowels = function(string) {
-    // Your code here 
-};
+// let hasThreeVowels = function(string) {
+//     let vowels = "aeiou";
+//     let count = 0;
+//     let usedVowels = { a: false, e: false, i: false, o: false, u: false }
+//     let splitStr = string.split(' ');
+//     let filteredStr = splitStr.map(function(value){
+//         for (let char of value) {
+//             if(vowels.includes(char) && !usedVowels[char]) {
+//                 count++
+//                 usedVowels[char] = true;
+//             }
+//         }
+//         if(count >= 3){
+//             return true;
+//         }
+//         return false;
+//     })
+//     return filteredStr.join(' ');
+// };
+function hasThreeVowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const uniqueVowels = [];
+    
+    str.toLowerCase().split('').forEach(char => {
+      if (vowels.includes(char) && !uniqueVowels.includes(char)) {
+        uniqueVowels.push(char);
+      }
+    });
+    
+    return uniqueVowels.length >= 3;
+  }
 
-// Your code here 
 
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
