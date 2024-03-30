1c9b4ae9772cbcdc734a93439e88bfc566246d2a
Cannicakee <stanfill11@gmail.com>
2024-03-30 14:08:18 -0600

    Problem 3 Solution

--- problems/03-long-words-filter.js
+++ problems/03-long-words-filter.js
@@ -14,9 +14,15 @@ console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // 
 */
 
 let longWords = function(words) {
-    // Your code here 
+ let filteredWord = words.filter(function(word){
+    return word.length > 5;
+ })
+ return filteredWord;
 };
 
+
+console.log(longWords(['bike', 'skateboard','scooter', 'moped'])); // [ 'skateboard', 'scooter' ]
+console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
