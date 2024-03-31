3a05797560102bdb686c89ba0fd4aeb0e23f946f
Cannicakee <stanfill11@gmail.com>
2024-03-31 13:44:41 -0600

    Incomplete Problem 16

--- problems/16-hipsterfy.js
+++ problems/16-hipsterfy.js
@@ -14,11 +14,19 @@ console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls
 */
 
 let removeLastVowel = function(word) {
-    // Your code here 
+    let vowels = "aeiou"
+    
+    for(let i = word.length -1; i >= 0; i--) {
+        let letter = word[i];
+            if(vowels.includes(letter.toLowerCase())) {
+                return word.split(letter).join('')
+            }
+    }
+    return word;
 };
 
 let hipsterfy = function(sentence) {
-    // Your code here 
+    return sentence.split(' ').map(removeLastVowel).join(' ')
 };
 
 // alternative solution using Array.map
@@ -26,6 +34,9 @@ let hipsterfy = function(sentence) {
 //     return sentence.split(' ').map(removeLastVowel).join(' ');
 // };
 
+console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
+// console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+// console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
